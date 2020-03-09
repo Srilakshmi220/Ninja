2e3973c1318ad5f4b3ab72b9497b4b675bd6c95f
Srilakshmi220 <61973003+Srilakshmi220@users.noreply.github.com>
2020-03-09 17:42:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+  
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const { mongoose } = require('./db.js');
+var employeeController = require('./controllers/employeeController.js');
+var user_loginController = require('./controllers/User_LoginController.js');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: 'http://localhost:4200' }));
+
+app.listen(3000, () => console.log('Server started at port : 3000'));
+
+
+app.use('/employees', employeeController);
+
+app.use('/user_login', user_loginController);
+
+var MongoClient = require('mongodb').MongoClient;
